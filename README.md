5481d21b753bd3a293943d80a4242cf063d70d12
Christian Sunesson <christian.sunesson@kivra.com>
2024-09-24 14:56:19 +0200

    fix: pick up http.route value

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Cowboy opentelemetry plugin
 
-Add cowboy_otel_h first to your cowboy stream handlers, it will pick up propagated spans to cowboy, and surround the request with a span.
-Also add cowboy_otel_middleware first to your cowboy middlewares, it will attach the created span in the stream handler in ranch process executing, allowing subspans to be created 
+Add `cowboy_otel_h` first to your cowboy stream handlers, it will pick up propagated spans to cowboy,
+and surround the request with a span.
+Also add `cowboy_otel_middleware` after your `cowboy_router` middlewares, it will attach the created
+span from the stream process in the process executing middlewares and your cowboy handler, so subspans are connected.
 
 ```erlang
     ProtocolOpts = #{
         stream_handlers => [cowboy_otel_h, cowboy_stream_h],
-        middlewares => [cowboy_otel_middleware, cowboy_router, cowboy_handler],
+        middlewares => [cowboy_router, cowboy_otel_middleware, cowboy_handler],
         ...
     }
 ```
--- src/cowboy_otel_middleware.erl
+++ src/cowboy_otel_middleware.erl
@@ -9,10 +9,54 @@
 -include_lib("opentelemetry_api/include/opentelemetry.hrl").
 
 -spec execute(Req, Env) -> {ok, Req, Env} when
-    Req :: cowboy_req:req(), Env :: cowboy_middleware:env().
+    Req :: cowboy_req:req(),
+    Env :: cowboy_middleware:env().
 execute(#{otel_ctx := Ctx, otel_span_ctx := SpanCtx} = Req, Env) ->
     _ = otel_ctx:attach(Ctx),
     ?set_current_span(SpanCtx),
+    case http_route(Req) of
+        undefined ->
+            ok;
+        HttpRoute ->
+            HttpMethod = http_method(Req),
+            otel_span:set_attribute(SpanCtx, 'http.route', HttpRoute),
+            NewName = iolist_to_binary([HttpMethod, " ", HttpRoute]),
+            otel_span:update_name(SpanCtx, NewName)
+    end,
     {ok, Req, Env};
 execute(Req, Env) ->
     {ok, Req, Env}.
+
+http_method(#{method := Method}) -> Method.
+
+http_route(#{path := Path, bindings := Bindings}) ->
+    RouteFun = fun
+        (_, <<>>, Acc) -> Acc;
+        (K, V, Acc) -> binary:replace(Acc, V, <<":", (atom_to_binary(K, utf8))/binary>>)
+    end,
+    maps:fold(RouteFun, Path, Bindings);
+http_route(_) ->
+    undefined.
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+test_routes() ->
+    [
+        {'_', [
+            {<<"/metrics">>, no_args, #{}},
+            {<<"/user/:uid/stuff">>, uid_args, #{}}
+        ]}
+    ].
+
+http_route_test() ->
+    Dispatch = cowboy_router:compile(test_routes()),
+    Req0 = #{host => <<"example.com">>, path => <<"/user/123/stuff">>},
+    Env0 = #{dispatch => Dispatch},
+    {ok, Req, Env} = cowboy_router:execute(Req0, Env0),
+    ?assertMatch(#{handler := uid_args}, Env),
+    ?assertMatch(#{path := <<"/user/123/stuff">>}, Req),
+    ?assertMatch(#{bindings := #{uid := <<"123">>}}, Req),
+    ?assertEqual(<<"/user/:uid/stuff">>, http_route(Req)).
+
+-endif.
