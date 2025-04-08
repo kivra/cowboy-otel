0c38e6866eb3b184205697c829783e928d62d3c0
Christian Sunesson <christian.sunesson@kivra.com>
2025-04-08 12:08:41 +0200

    fix: drop span when server kind and member of set only

--- src/cowboy_otel_sampler.erl
+++ src/cowboy_otel_sampler.erl
@@ -6,6 +6,7 @@
 -export([setup/1]).
 -export([should_sample/7]).
 
+-include_lib("opentelemetry_api/include/opentelemetry.hrl").
 -include_lib("opentelemetry/include/otel_sampler.hrl").
 
 setup(BinList) when is_binary(hd(BinList)) ->
@@ -17,7 +18,7 @@ description(#{exclude := List}) ->
     iolist_to_binary(io_lib:format("Exclude spans from set ~p", [List])).
 
 should_sample(_Ctx, _TraceId, _Links, Name, Kind, _Attrs, #{exclude := List}) ->
-    case Kind == server andalso not lists:member(Name, List) of
-        true -> {?RECORD_AND_SAMPLE, [], []};
-        _ -> {?DROP, [], []}
+    case Kind == ?SPAN_KIND_SERVER andalso lists:member(Name, List) of
+        true -> {?DROP, [], []};
+        _ -> {?RECORD_AND_SAMPLE, [], []}
     end.
